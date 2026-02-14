42e08ea84d560e6b30dfa1e4a4f178c6a75852e2
skai273 <kinoshita116@gmail.com>
2026-02-14 23:24:47 +0900

    fix failed tests of catr

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -57,19 +57,20 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
+    let mut line_num = 0;
     for filename in config.files {
         match open(&filename) {
             Err(err) => eprintln!("Failed to open {}: {}", filename, err),
             Ok(file) => {
-                let mut last_num = 0;
-                for (line_num, line) in file.lines().enumerate() {
+                for line in file.lines() {
                     let line = line?;
                     if config.number_lines {
-                        println!("{:>6}\t{}", line_num + 1, line);
+                        line_num += 1;
+                        println!("{:>6}\t{}", line_num, line);
                     } else if config.number_nonblank_lines {
                         if !line.is_empty() {
-                            last_num += 1;
-                            println!("{:>6}\t{}", last_num, line);
+                            line_num += 1;
+                            println!("{:>6}\t{}", line_num, line);
                         } else {
                             println!();
                         }
