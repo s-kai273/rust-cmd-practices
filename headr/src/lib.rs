585f56ac4ff8a3b93ff0a49ba0458d4d31e979c6
skai273 <kinoshita116@gmail.com>
2026-03-04 23:53:09 +0900

    implement file read logic of headr

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,9 @@
 use clap::{App, Arg};
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -81,7 +85,19 @@ fn test_parse_positive_int() {
     assert_eq!(res.unwrap_err().to_string(), "0".to_string());
 }
 
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(_) => println!("Opend {}", filename),
+        }
+    }
     Ok(())
 }
